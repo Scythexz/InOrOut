d03cd35cddaf3d4fab30492ea2c68367b24e2dbb
Kalyr <89013077+Kalyr@users.noreply.github.com>
2023-12-23 09:41:59 +0800

    added css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import "../styles/Login.css";
+import logo from '../img/inorout.png';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -21,29 +23,96 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form>
-        <label>
-          Email:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
 
-      <p>
-        Don't have an account? <Link to="/registration">Register here</Link>.
-      </p>
-    </div>
+
+    <div class = 'container flex'>
+      <div class ='login-page flex'>
+        <div class ='text'>
+          <div class='logo'>
+          <img src={logo}/>
+          </div>
+          <p class="slogan">Save time, no hassle. Class alerts in a flash</p>
+        </div>
+            <form>
+            <h1 class="loginh1">Login Page</h1>
+            <hr></hr>
+              <label>
+                Email:
+                </label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+              
+              <br />
+              <label>
+                Password:
+                </label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              
+              <br />
+              <button type="button" class="loginbutton"onClick={handleLogin}>
+                Login
+              </button>
+              <p class="noacc">
+              Don't have an account? </p>
+              <p class="noacc"><Link to="/registration">Register here</Link>
+              </p>
+            </form>
+        </div>
+      </div>
+
+
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div class="container flex">
+    //   <div class="facebook-page flex">
+    //     <div class="text">
+    //       <h1>facebook</h1>
+    //       <p>Connect with friends and the world </p>
+    //       <p> around you on Facebook.</p>
+    //     </div>
+    //     <form action="#">
+    //       <input type="email" placeholder="Email or phone number" required>
+    //       <input type="password" placeholder="Password" required>
+    //       <div class="link">
+    //         <button type="submit" class="login">Login</button>
+    //         <a href="#" class="forgot">Forgot password?</a>
+    //       </div>
+    //       <hr>
+    //       <div class="button">
+    //         <a href="#">Create new account</a>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
+
+
 
 export default Login;
