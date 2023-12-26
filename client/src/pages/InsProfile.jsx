2bedb8ef9aa02c34736f67b7b352ce60bd0725e1
Shintoburiburi <leigjhan@gmail.com>
2023-12-26 20:41:51 +0800

    mini

--- client/src/pages/InsProfile.jsx
+++ client/src/pages/InsProfile.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import InsProfileCard from '../components/insProfileCard';
+import { jwtDecode } from "jwt-decode";
 
 function InsProfile() {
+
+  const token = sessionStorage.getItem('token');
+  console.log('Undecoded Token:', token);
+  const decoded = jwtDecode(token);
+  console.log('Decoded:', decoded);
+
   return (
     <div>
       <Navbar />
       <h1>Instructor's Profile Page</h1>
-      <h1>Welcome, {'<'}instructor{'>'}</h1>
+      <h1>Welcome, {decoded.full_name}</h1>
       <p>Info about you</p>
       {/* Add your content for the home page */}
       {/* Render the InsProfileCard component */}
